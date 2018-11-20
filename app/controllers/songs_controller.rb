0e048bc144a5c1b051f1fb4db0d024c722ffe15c
Trac Nguyen <nguyendtrac@gmail.com>
2018-11-20 02:16:32 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
   end
 
   def upload
-    SongsWorker.perform_async(params[:songs].path)
+    SongsWorker.perform_async(params[:song].path)
     redirect_to songs_path
   end
 
